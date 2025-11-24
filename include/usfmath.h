9a2562c74a2c43ade75d1a5de8e6d8d52a23686e
lologar1 <guertin.laurent@gmail.com>
2025-11-23 21:52:27 -0500

    Math comparison headers

--- include/usfmath.h
+++ include/usfmath.h
@@ -3,6 +3,10 @@
 
 #include <time.h>
 
+#define USF_MIN(X, Y) ((X) <= (Y) ? (X) : (Y))
+#define USF_MAX(X, Y) ((X) > (Y) ? (X) : (Y))
+#define USF_CLAMP(X, LOW, HIGH) ((X) < (LOW) ? (LOW) : ((X) > (HIGH) ? (HIGH) : (X)))
+
 double usf_elapsedtimes(struct timespec start, struct timespec end);
 double usf_elapsedtimens(struct timespec start, struct timespec end);
 
